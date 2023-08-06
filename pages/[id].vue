30a6e1036aacf580e2032902ea0ea0cc7ec274b4
mehran7775 <u3efimehran@gmail.com>
2023-08-06 21:21:58 +0330

    init commit on this branch

---
+++ pages/[id].vue
@@ -0,0 +1,18 @@
+<template>
+   id: {{ $route.params.id }}
+</template>
+
+
+<script setup lang="ts">
+
+useLazyAsyncData(() => {
+   
+})
+
+</script>
+
+
+<style lang="postcss" scoped>
+
+
+</style>
